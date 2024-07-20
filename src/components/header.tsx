86a634e2ff3ba25409ce17e3676f5f90cef98446
JustACodeMonkey <JustACodeMonkey@users.noreply.github.com>
2024-07-20 11:00:01 -0400

    Added sticky header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import { HeaderFooterCommon } from './header-footer-common';
 
 export const Header = () => {
   return (
-    <header className="bg-blue-500 flex flex-col sm:flex-row justify-between items-start sm:items-end gap-2 p-4 sm:px-center-screen w-full">
+    <header className="bg-blue-500 flex flex-col sm:flex-row justify-between items-start sm:items-end gap-2 p-4 sm:px-center-screen w-full sticky top-0 z-10">
       <HeaderFooterCommon />
     </header>
   );
