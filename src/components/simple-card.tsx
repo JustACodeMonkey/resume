e81044ad1ad742584f642c2654032e626161b452
JustACodeMonkey <JustACodeMonkey@users.noreply.github.com>
2024-08-19 10:41:07 -0400

    Border shading and tag line color

--- src/components/simple-card.tsx
+++ src/components/simple-card.tsx
@@ -9,7 +9,7 @@ interface SimmpleCardProps {
 
 export const SimpleCard = ({ children, className, title }: SimmpleCardProps) => {
   return (
-    <section className={"border-y border-opacity-20 bg-white/80 dark:bg-black/80 p-2 rounded-lg min-w-60 space-y-4 drop-shadow-md " + className}>
+    <section className={"border-y-2 border-black/25 dark:border-white/40 bg-white/80 dark:bg-black/80 p-2 rounded-lg min-w-60 space-y-4 drop-shadow-md " + className}>
       {title && (
         <h2 className={silkscreen.className + " text-blue-700 dark:text-blue-500 font-semibold text-xl uppercase"}>{title}</h2>
       )}
--- src/components/tag-line.tsx
+++ src/components/tag-line.tsx
@@ -4,7 +4,7 @@ export const TagLine = () => {
   return (
     <p className={silkscreen.className 
       + " flex flex-col justify-center items-center text-center min-h-[calc(100vh_-_166px)] text-4xl sm:text-6xl"
-      + " [text-shadow:_3px_3px_4px_#000] text-balance"
+      + " [text-shadow:_3px_3px_4px_#000] text-balance text-white"
     }>
       Biker, Skier, Drummer, but mostly an introverted code nerd.
     </p>
